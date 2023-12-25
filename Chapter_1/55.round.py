4b2e0d550458ad05c6821b36f13b73deb4c9cb97
D_S_2023 <975ds579@gmail.com>
2023-12-25 18:58:29 +0300

    (laptop)_add_55.round.py

---
+++ Chapter_1/55.round.py
@@ -0,0 +1,8 @@
+number = 12.7
+print(round(number))
+numberProposal = float(input('Введите число с плавающей точкой: '))
+print(f'Вы ввели: ',numberProposal,
+      '\n Округлим его до:', round(numberProposal))
+
+
+
